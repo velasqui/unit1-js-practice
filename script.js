071f41017fdef66a75889080c7a5a5ec49f717bf
Ian velasquez <98791481+velasqui@users.noreply.github.com>
2022-03-15 22:16:26 -0400

    mid-way updates

--- script.js
+++ script.js
@@ -28,3 +28,39 @@ console.log(`2) ${travelOptions[1]}`);
 console.log(`3) ${travelOptions[2]}`);
 console.log(`4) ${travelOptions[3]}`);
 
+var travelType = prompt("How would you like to travel?");
+
+console.log("Travel Type: " + travelType);
+
+let distance = 100;
+let time = 0;
+let cost = 0;
+
+if (travelType === "foot") {
+    console.log("Walking is free! Speed is 3mph.");
+    cost = 0;
+    time = distance / 3;
+} else if (travelType === "bike") {
+    var rentBike = prompt("Do you need to rent a bike? (yes or no)")
+    if (rentBike === "yes") {
+        console.log("Bike rental is $45! Speed is 10mph.");
+        cost = 45;
+    } else {
+        console.log("Biking is free! Speed is 10mph.");
+        cost = 0;
+    }
+    time = distance / 10;
+} else if (travelType === "car") {
+    console.log("Driving is $0.25/mi. Speed is 60mph.");
+    cost = 0.25 * distance;
+    time = distance / 60;
+} else if (travelType === "airplane") {
+    var passengerCount = prompt("How many passengers?");
+    console.log("Flying is $0.10/mi. Speed is 400mph.");
+    cost = 0.10 * distance * passengerCount;
+    time = distance / 400;
+} else {
+    console.log("Sorry. " + travelType + " isn't an option.");
+}
+
+console.log(`Traveling ${distance} miles by ${travelType} took ${time} hours and cost $${cost}!`);
